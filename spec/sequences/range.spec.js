cd60fa9a7b55190996b90b0ad9e928a4ab9b21be
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2017-05-14 13:48:52 +1000

    Test and document the range generator

--- spec/sequences/range.spec.js
+++ spec/sequences/range.spec.js
@@ -1,5 +1,9 @@
-describe("Sequences", () => {
+describe("Range sequence", () => {
 	const iter = require("../../index");
+
+	it("should generate an empty if no parameters are given", () => {
+		expect([...iter.range()]).toEqual([]);
+	});
 
 	it("should generate a range from 0 to 9", () => {
 		expect([...iter.range(10)]).toEqual([0,1,2,3,4,5,6,7,8,9]);
@@ -7,5 +11,13 @@ describe("Sequences", () => {
 
 	it("should generate a range from 1 to 10", () => {
 		expect([...iter.range(1, 11)]).toEqual([1,2,3,4,5,6,7,8,9,10]);
+	});
+
+	it("should count down if a single negative value is given", () => {
+		expect([...iter.range(-10)]).toEqual([0,-1,-2,-3,-4,-5,-6,-7,-8,-9]);
+	});
+
+	it("should increment by 2", () => {
+		expect([...iter.range(0, 11, 2)]).toEqual([0,2,4,6,8,10]);
 	});
 });
