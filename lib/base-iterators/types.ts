113fb2f9ff1c068563fc795c726eff1f04235a40
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2023-05-04 13:55:36 +1000

    Define AnyIterable type

--- lib/base-iterators/types.ts
+++ lib/base-iterators/types.ts
@@ -2,3 +2,5 @@
 export interface Iterable<T, TReturn = any, TNext = undefined> {
     [Symbol.iterator](): Iterator<T, TReturn, TNext>;
 }
+
+export type AnyIterable<T, TReturn, TNext> = Iterable<T, TReturn, TNext> | ArrayLike<T> | Generator<T, TReturn, TNext>;
