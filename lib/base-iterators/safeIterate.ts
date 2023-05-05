e140ed8cd623fcf3e8efdeec1db3e46a7ae0c750
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2023-05-05 11:38:20 +1000

    Update safeIterate with better types

--- lib/base-iterators/safeIterate.ts
+++ lib/base-iterators/safeIterate.ts
@@ -1,20 +1,67 @@
+import { isIterable } from "./isIterable";
 import { iterate } from "./iterate";
+import { AnyIterable, GeneratorReturnType, Iterable } from "./types";
 
 /**
- * Generator that yields values from the provided object's iterator via [Symbol.iterator],
- * but prevents a return call propagating to the wrapped iterator when when this generator is ended.
+ * Delegates iteration to the provided Generator, but prevents a return call propagating to the wrapped iterator when
+ * this generator is ended.
  *
- * @generator
- * @param  {Iterable} it Any iterable object
- * @yields {*} Values from the provided object's iterator
+ * @param it Any generator.
+ * @yields Values from the provided generator.
  */
-export function* safeIterate<T>(it: Iterable<T> | ArrayLike<T>) {
-    const source = iterate(it);
-    let next;
+export function safeIterate<T, TReturn, TNext>(it: Generator<T, TReturn, TNext>): typeof it;
 
-    while (true) {
-        next = source.next();
-        if (next.done) return next.value;
-        yield next.value;
+/**
+ * Delegates iteration to the provided object's iterator via `[Symbol.iterator]`, but prevents a return call propagating
+ * to the wrapped iterator when when this generator is ended.
+ *
+ * @param it Any iterable object.
+ * @yields Values from the provided object's iterator.
+ */
+export function safeIterate<T, TReturn, TNext>(
+    it: Iterable<T, TReturn, TNext>
+): ReturnType<(typeof it)[typeof Symbol.iterator]> extends Iterator<infer TYield, infer TReturn, infer TNext>
+    ? Generator<TYield, TReturn, TNext>
+    : never;
+
+/**
+ * Iterates over the object's indexed values.
+ *
+ * @param it Any Array-like object.
+ * @yields The object's indexed values.
+ */
+export function safeIterate<T>(it: ArrayLike<T>): Generator<T, void, undefined>;
+
+/**
+ * Delegates iteration to the provided Generator or the provided object's iterator,
+ * or iterates over the object's indexed values.
+ *
+ * @param it Any Generator, Iterable or Array-like object
+ * @yields Values from the provided generator or object's iterator, or its indexed values.
+ */
+export function safeIterate<T, TReturn, TNext>(
+    it: AnyIterable<T, TReturn, TNext>
+): GeneratorReturnType<typeof it, T, TReturn, TNext>;
+
+export function* safeIterate<T, TReturn, TNext>(it: AnyIterable<T, TReturn, TNext>) {
+    if (isIterable(it)) {
+        const iterator = iterate(it);
+
+        let nextInput: TNext;
+
+        let result: IteratorResult<T, TReturn | void>;
+        for (result = iterator.next(); !result.done; result = iterator.next(nextInput)) {
+            nextInput = (yield result.value) as TNext;
+        }
+        return result.value;
+    } else {
+        const iterator = iterate(it);
+
+        while (true) {
+            const { done, value } = iterator.next();
+            if (done) return;
+
+            yield value;
+        }
     }
 }
