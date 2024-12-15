b1c56e53a9678126d64ddd0e301b5f44ad1595b9
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2024-12-16 10:02:32 +1100

    Convert chunk test to typescript

--- lib/chainables/chunk.spec.ts
+++ lib/chainables/chunk.spec.ts
@@ -1,8 +1,8 @@
-const { chunk } = require("../../");
+import { chunk } from "./chunk";
 
 describe("chunk generator", () => {
     it("should yield 2 values at a time", () => {
-        let iterator = chunk([0, 1, 2, 3, 4, 5], 2);
+        const iterator = chunk([0, 1, 2, 3, 4, 5], 2);
 
         expect(iterator.next().value).toEqual([0, 1]);
         expect(iterator.next().value).toEqual([2, 3]);
